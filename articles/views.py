8aaa60c3ca2020bfaf6fa082d308a4bd8b3ff0f9
Nitesh-mint <nitesh.raya058@gmail.com>
2022-10-23 11:52:13 +0545

    added functionality to add news posts from the page

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import UpdateView, DeleteView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
 
 from .models import Article
@@ -7,7 +7,7 @@ from .models import Article
 # Create your views here.
 class ArticleListView(ListView):
     model = Article
-    template_name = 'article_list.html'
+    template_name = 'article_list.html' 
 
 class ArticleDetailView(DetailView):
     model = Article
@@ -22,3 +22,13 @@ class ArticleDeleteView(DeleteView):
     model = Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
+
+class ArticleCreateView(CreateView):
+    model  = Article
+    template_name = 'article_new.html'
+    fields = ('title', 'body',)
+    success_url = reverse_lazy('article_list')
+
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
